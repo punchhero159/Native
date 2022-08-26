4ccc6cd1a404d5fa91ce0e319badf18d9ae950f8
Piyawat Saelim <6310110285@psu.ac.th>
2022-08-27 02:03:38 +0700

    third

--- wt-app/components/Weather.js
+++ wt-app/components/Weather.js
@@ -1,6 +1,6 @@
 import { Text, ImageBackground, StyleSheet } from 'react-native'
 import Forecast from './Forecast'
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
 
 export default function Weather(props) {
@@ -9,6 +9,24 @@ export default function Weather(props) {
         description: 'description',
         temp: 0
 })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=d49581ca391fd35d3e182af30f6fdeb5`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+            });
+            })
+        .catch((error) => {
+            console.warn(error);
+        });
+        }
+    }, [props.zipCode])
     
     return (
         <ImageBackground source={require('../onepiece1.jpg')} style={styles.backdrop}>
