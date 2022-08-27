23637a6ac974ded7b115f837f7a0d9c9e37a751f
Piyawat Saelim <6310110285@psu.ac.th>
2022-08-27 20:11:04 +0700

    sixth

--- wt-app/screen/ZipCodeScreen.js
+++ wt-app/screen/ZipCodeScreen.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { FlatList, View, Text } from 'react-native'
+import { FlatList, View, Text, StyleSheet } from 'react-native'
+import { TouchableHighlight } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
 
 const availableZipItems = [
     { place: 'Hatyai', code: '90110' },
@@ -7,29 +9,51 @@ const availableZipItems = [
     { place: 'Chiangmai', code: '50000' },
     { place: 'Khonkaen', code: '40000' },
     { place: 'Chonburi', code: '20000' },
-]
+    { place: 'TaSongYarn', code: '63150' },
+    { place: 'Pattanee', code: '94000' },
+    { place: 'Nontaburi', code: '11000' },
+    { place: 'RaYong', code: '21000' },
+    { place: 'Pattaya', code: '20260' },
+   ]
+   
 
 const ZipItem = ({place, code, navigation}) => (
-    <TouchableHighlight onPress={() => navigation.navigate('Weather', { zipCode: code})}>
-    <View>
-    <Text>{place}</Text>
-    <Text >{code}</Text>
-    </View>
+    <TouchableHighlight onPress={() => {
+        navigation.navigate('Weather', {zipCode: code})
+    }}>
+        <View style = {style.zipItem}>
+            <Text style = {style.zipPlace}>{place}</Text>
+            <Text style = {style.zipCode}>{code}</Text>
+        </View>
     </TouchableHighlight>
-    )
-    
-   const _keyExtractor = item => item.code
+)
 
-export default function ZipCodeScreen(){
+export default function ZipCodeScreen() {
     const navigation = useNavigation()
     return (
-        <View>
         <FlatList
-            data={availableZipItems}
-            keyExtractor={_keyExtractor}
-            renderItem={({item}) => <ZipItem {...item} navigation={navigation}/>}
+            data = {availableZipItems}
+            keyExtractor = {item => item.code}
+            renderItem = {({item}) => <ZipItem {...item} navigation={navigation}/>}
         />
-        <StatusBar style="auto" />
-        </View>
-    );
-   }
+    )
+}
+
+const style = StyleSheet.create ({
+    zipItem: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: 25,
+    },
+    zipPlace: {
+        flex: 1,
+        fontSize: 15,
+
+    },
+    zipCode: {
+        flex: 1,
+        fontSize: 15,
+        paddingLeft: 100
+    }
+});
