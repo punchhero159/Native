969f8436ee8ec543a5692b3b5e9f1e774c8a2ac6
Piyawat Saelim <6310110285@psu.ac.th>
2022-08-27 20:40:16 +0700

    eighth

--- wt-app/components/Forecast.js
+++ wt-app/components/Forecast.js
@@ -4,24 +4,30 @@ import { View, Text, StyleSheet } from 'react-native'
 export default function Forecast(props) {
     return (
         <View>
-            <Text style={styles.message}>{props.main}</Text>
-            <Text style={styles.message2}>{props.description}</Text>
-            <Text style={styles.message}>{props.temp}°C</Text>
+            <Text style={styles.mainpartstyle}>{props.main}</Text>
+            <Text style={styles.despartstyle}>{props.description}</Text>
+            <Text style={styles.temppartstyle}>{props.temp}°C</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
 
-    message: {
+    mainpartstyle: {
+        color: 'white',
+        fontSize: 50,
+        textAlign: 'center'
+    },
+    despartstyle:{
+        color: 'white',
+        fontsize: 20,
+        fontStyle: 'italic',
+        textAlign: 'center' 
+    },
+    temppartstyle: {
         color: 'white',
         fontSize: 40,
         textAlign: 'center'
-    },
-    message2:{
-        color: 'white',
-        fontsize: 20,
-        textAlign: 'center' 
     }
 });
 
